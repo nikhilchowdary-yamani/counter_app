221f620a9c40a2e7bc00c68b6a5ea11232e36c55
nikhilchowdary-yamani <nikhilchowdary_yamani@srmap.edu.in>
2025-02-08 14:06:06 -0500

    Add fade transition animation to image toggle.

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Counter'),
+      home: const MyHomePage(title: 'Counter and Image Toggle'),
     );
   }
 }
@@ -29,10 +29,29 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   int _counter = 0;
   bool _isImage1 = true;
+  late AnimationController _animationController;
+  late CurvedAnimation _curvedAnimation;
 
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _curvedAnimation =
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -43,6 +62,8 @@ class _MyHomePageState extends State<MyHomePage> {
   void _toggleImage() {
     setState(() {
       _isImage1 = !_isImage1;
+      _animationController.reset();
+      _animationController.forward();
     });
   }
 
@@ -56,14 +77,24 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // ... (Counter display)
-            Image.asset(
-              _isImage1 ? 'assets/image1.png' : 'assets/image2.png',
-              height: 200,
-              width: 200,
+            Text(
+              'Counter: $_counter',
+              style: const TextStyle(fontSize: 24),
             ),
             const SizedBox(height: 20),
-            // ... (Increment button)
+            FadeTransition(
+              opacity: _curvedAnimation,
+              child: Image.asset(
+                _isImage1 ? 'assets/image1.png' : 'assets/image2.png',
+                height: 200,
+                width: 200,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: const Text('Increment'),
+            ),
             ElevatedButton(
               onPressed: _toggleImage,
               child: const Text('Toggle Image'),
